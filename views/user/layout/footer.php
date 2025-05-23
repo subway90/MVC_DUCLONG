03d8b3d8c985c1bfef2e0446118dcf661fcca93f
subway90 <nguyenhieu3105@gmail.com>
2025-05-23 18:01:32 +0700

    update footer

--- views/user/layout/footer.php
+++ views/user/layout/footer.php
@@ -21,90 +21,91 @@
 </div>
 
 <!-- Footer Start -->
-<footer class="container bg-light border border-3 border-bottom-0 border-gold p-3 rounded-3 rounded-bottom-0 mt-5">
-    <div class="row">
-        <div class="col-12 col-lg-6">
-            <div class="d-flex align-items-center justify-content-center justify-content-lg-start gap-3">
-                <img width="40" src="<?= URL_STORAGE ?>system/logo.png" alt="DUCLONG LOGO">
-                <div class="navbar-brand fw-bold">DUCLONG GLOBAL CO.,LTD</div>
-            </div>
-            <div class="d-flex align-items-center align-items-lg-start flex-column gap-2 mt-3">
-                <div class="">
-                    <i class="fas fa-map-marker-alt me-1 text-brown"></i>
-                    <span class="text-muted small">
-                        36/80 Xuân Thuỷ, Quận Nam Từ Liêm, TP. Hà Nội
-                    </span>
+<footer class="container my-5">
+    <div class="border-mix-gold">
+        <div class="d-flex flex-column flex-lg-row justify-content-between bg-light p-3 rounded-3">
+            <div class="col-12 col-lg-5">
+                <div class="d-flex align-items-center justify-content-center justify-content-lg-start gap-3">
+                    <img width="40" src="<?= URL_STORAGE ?>system/logo.png" alt="DUCLONG LOGO">
+                    <div class="navbar-brand fw-bold">DUCLONG GLOBAL CO.,LTD</div>
                 </div>
-                <div class="w-100">
-                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1314.5507837524422!2d105.78871915249151!3d21.037547932773276!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3135ab3613967dbf%3A0x3e1ad0c809d813b7!2sAcademy%20of%20Journalism%20and%20Communication!5e1!3m2!1sen!2s!4v1747822628667!5m2!1sen!2s" width="100%" height="240" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                <div class="d-flex align-items-center align-items-lg-start flex-column gap-2 mt-3">
+                    <div class="">
+                        <i class="fas fa-map-marker-alt me-1 text-brown"></i>
+                        <span class="text-muted small">
+                            36/80 Xuân Thuỷ, Quận Nam Từ Liêm, TP. Hà Nội
+                        </span>
+                    </div>
+                    <div class="w-100">
+                        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1314.5507837524422!2d105.78871915249151!3d21.037547932773276!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3135ab3613967dbf%3A0x3e1ad0c809d813b7!2sAcademy%20of%20Journalism%20and%20Communication!5e1!3m2!1sen!2s!4v1747822628667!5m2!1sen!2s" width="100%" height="240" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-12 col-lg-5 d-flex flex-column ps-lg-5 mt-3 mt-lg-0">
+
+                <div class="col-12 d-flex">
+                    <div class="col-6 text-center text-lg-start">
+                        <h5 class="text-brown fw-bold">
+                            Liên hệ
+                        </h5>
+                        <div class="d-flex flex-column mt-4 gap-1">
+                            <div class="small">
+                                Email : info@duclong.com
+                            </div>
+                            <div class="small">
+                                Điện thoại : 0948594444
+                            </div>
+                        </div>
+                        <div class="d-flex justify-content-center justify-content-lg-start gap-3 mt-2">
+                            <a href="#" class="fs-3 text-decoration-none text-dark fw-semibold">
+                                <i class="fab fa-facebook"></i>
+                            </a>
+                            <a href="#" class="fs-3 text-decoration-none text-dark fw-semibold">
+                            <i class="fab fa-youtube"></i>
+                            </a>
+                            <a href="#" class="fs-3 text-decoration-none text-dark fw-semibold">
+                                <i class="fab fa-twitter"></i>
+                            </a>
+                        </div>
+                    </div>
+
+                    <div class="col-6 text-center text-lg-start">
+                        <h5 class="text-brown fw-bold">
+                            Dịch vụ cung cấp
+                        </h5>
+                        <div class="d-flex flex-column mt-4 gap-1">
+                            <a href="#" class="text-decoration-none text-dark small">Thiết kế website chuyên nghiệp</a>
+                            <a href="#" class="text-decoration-none text-dark small">Phòng marketing thuê ngoài</a>
+                            <a href="#" class="text-decoration-none text-dark small">Quản trị và sáng tạo nội dung</a>
+                            <a href="#" class="text-decoration-none text-dark small">Thiết kế nhận diện thương hiệu</a>
+                            <a href="#" class="text-decoration-none text-dark small">Thiết kế Landing page</a>
+                            <a href="#" class="text-decoration-none text-dark small">Dịch vụ sale tổng thể</a>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-12 d-flex my-3">
+                    <div class="col-6 text-center text-lg-start">
+                        <div class="d-flex flex-column mt-4 gap-1">
+                            <a href="#" class="text-decoration-none text-dark small">X</a>
+                            <a href="#" class="text-decoration-none text-dark small">Linkedln</a>
+                            <a href="#" class="text-decoration-none text-dark small">Facebook</a>
+                            <a href="#" class="text-decoration-none text-dark small">Youtube</a>
+                        </div>
+                    </div>
+
+                    <div class="col-6 text-center text-lg-start">
+                        <div class="d-flex flex-column mt-4 gap-1">
+                            <a href="#" class="text-decoration-none text-dark small">FAQ</a>
+                            <a href="#" class="text-decoration-none text-dark small">Term & Conditions</a>
+                            <a href="#" class="text-decoration-none text-dark small">Privacy Policy</a>
+                            <a href="#" class="text-decoration-none text-dark small">Refund Policy</a>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
-
-        <div class="col-12 col-lg-6 d-flex flex-column ps-lg-5 mt-3 mt-lg-0">
-
-            <div class="col-12 d-flex">
-                <div class="col-6 text-center text-lg-start">
-                    <h5 class="text-brown fw-bold">
-                        Liên hệ
-                    </h5>
-                    <div class="d-flex flex-column mt-4 gap-1">
-                        <div class="small">
-                            Email : info@duclong.com
-                        </div>
-                        <div class="small">
-                            Điện thoại : 0948594444
-                        </div>
-                    </div>
-                    <div class="d-flex justify-content-center justify-content-lg-start gap-3 mt-2">
-                        <a href="#" class="fs-3 text-decoration-none text-dark fw-semibold">
-                            <i class="fab fa-facebook"></i>
-                        </a>
-                        <a href="#" class="fs-3 text-decoration-none text-dark fw-semibold">
-                        <i class="fab fa-youtube"></i>
-                        </a>
-                        <a href="#" class="fs-3 text-decoration-none text-dark fw-semibold">
-                            <i class="fab fa-twitter"></i>
-                        </a>
-                    </div>
-                </div>
-
-                <div class="col-6 text-center text-lg-start">
-                    <h5 class="text-brown fw-bold">
-                        Dịch vụ cung cấp
-                    </h5>
-                    <div class="d-flex flex-column mt-4 gap-1">
-                        <a href="#" class="text-decoration-none text-dark small">Thiết kế website chuyên nghiệp</a>
-                        <a href="#" class="text-decoration-none text-dark small">Phòng marketing thuê ngoài</a>
-                        <a href="#" class="text-decoration-none text-dark small">Quản trị và sáng tạo nội dung</a>
-                        <a href="#" class="text-decoration-none text-dark small">Thiết kế nhận diện thương hiệu</a>
-                        <a href="#" class="text-decoration-none text-dark small">Thiết kế Landing page</a>
-                        <a href="#" class="text-decoration-none text-dark small">Dịch vụ sale tổng thể</a>
-                    </div>
-                </div>
-            </div>
-
-            <div class="col-12 d-flex my-3">
-                <div class="col-6 text-center text-lg-start">
-                    <div class="d-flex flex-column mt-4 gap-1">
-                        <a href="#" class="text-decoration-none text-dark small">X</a>
-                        <a href="#" class="text-decoration-none text-dark small">Linkedln</a>
-                        <a href="#" class="text-decoration-none text-dark small">Facebook</a>
-                        <a href="#" class="text-decoration-none text-dark small">Youtube</a>
-                    </div>
-                </div>
-
-                <div class="col-6 text-center text-lg-start">
-                    <div class="d-flex flex-column mt-4 gap-1">
-                        <a href="#" class="text-decoration-none text-dark small">FAQ</a>
-                        <a href="#" class="text-decoration-none text-dark small">Term & Conditions</a>
-                        <a href="#" class="text-decoration-none text-dark small">Privacy Policy</a>
-                        <a href="#" class="text-decoration-none text-dark small">Refund Policy</a>
-                    </div>
-                </div>
-            </div>
-        </div>
-
     </div>
 </footer>
 
